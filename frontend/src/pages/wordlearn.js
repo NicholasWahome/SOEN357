be26fd32800daaddbe88343f1fba9a8b3af28833
NicholasWahome <87381233+NicholasWahome@users.noreply.github.com>
2024-04-16 18:51:57 -0400

    Updated Styling

--- frontend/src/pages/wordlearn.js
+++ frontend/src/pages/wordlearn.js
@@ -13,107 +13,106 @@ import kiwi from '../img/kiwi.png';
 import NavigationBar from "../navigationBar/navigationBar";
 import Footer from "../component/footer";
 
-// Define an array of objects containing words and their associated images
 const words = [
-    { word: 'APPLE', image: apple, audio: 'audio/apple.mp3' },
-    { word: 'BANANA', image: banana, audio: 'audio/banana.mp3' },
-    { word: 'CHERRY', image: cherry, audio: 'audio/cherry.mp3' },
-    { word: 'WATERMELON', image: watermelon, audio: 'audio/watermelon.mp3' },
-    { word: 'ORANGE', image: orange, audio: 'audio/orange.mp3' },
-    { word: 'GRAPES', image: grapes, audio: 'audio/grapes.mp3' },
-    { word: 'LEMON', image: lemon, audio: 'audio/lemon.mp3' },
-    { word: 'STRAWBERRY', image: strawberry, audio: 'audio/strawberry.mp3' },
-    { word: 'PINEAPPLE', image: pineapple, audio: 'audio/pineapple.mp3' },
-    { word: 'MANGO', image: mango, audio: 'audio/mango.mp3' },
-    { word: 'KIWI', image: kiwi, audio: 'audio/kiwi.mp3' }
+  { word: 'APPLE', image: apple, audio: 'audio/apple.mp3' },
+  { word: 'BANANA', image: banana, audio: 'audio/banana.mp3' },
+  { word: 'CHERRY', image: cherry, audio: 'audio/cherry.mp3' },
+  { word: 'WATERMELON', image: watermelon, audio: 'audio/watermelon.mp3' },
+  { word: 'ORANGE', image: orange, audio: 'audio/orange.mp3' },
+  { word: 'GRAPES', image: grapes, audio: 'audio/grapes.mp3' },
+  { word: 'LEMON', image: lemon, audio: 'audio/lemon.mp3' },
+  { word: 'STRAWBERRY', image: strawberry, audio: 'audio/strawberry.mp3' },
+  { word: 'PINEAPPLE', image: pineapple, audio: 'audio/pineapple.mp3' },
+  { word: 'MANGO', image: mango, audio: 'audio/mango.mp3' },
+  { word: 'KIWI', image: kiwi, audio: 'audio/kiwi.mp3' }
 ];
 
 const WordLearn = () => {
-    const [typedWords, setTypedWords] = useState({});
-    const [score, setScore] = useState(0);
-    const [successWords, setSuccessWords] = useState({});
+  const [typedWords, setTypedWords] = useState({});
+  const [score, setScore] = useState(0);
+  const [successWords, setSuccessWords] = useState({});
 
-    // Load score and success words from local storage on component mount
-    useEffect(() => {
-        const storedScore = localStorage.getItem('score');
-        if (storedScore) {
-            setScore(parseInt(storedScore));
-        }
-        const storedSuccessWords = localStorage.getItem('successWords');
-        if (storedSuccessWords) {
-            setSuccessWords(JSON.parse(storedSuccessWords));
-        }
-    }, []);
+  useEffect(() => {
+    const storedScore = localStorage.getItem('score');
+    if (storedScore) {
+      setScore(parseInt(storedScore));
+    }
+    const storedSuccessWords = localStorage.getItem('successWords');
+    if (storedSuccessWords) {
+      setSuccessWords(JSON.parse(storedSuccessWords));
+    }
+  }, []);
 
-    const speakWord = (text) => {
-        const utterance = new SpeechSynthesisUtterance(text);
-        utterance.lang = 'en-US'; // Change to the appropriate language code if needed
-        window.speechSynthesis.speak(utterance);
-    };
+  const speakWord = (text) => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = 'en-US';
+    window.speechSynthesis.speak(utterance);
+  };
 
-    const checkWord = (word) => {
-        const typedWord = typedWords[word];
-        const correctWord = words.find(item => item.word === word);
-        if (typedWord && correctWord && typedWord.toLowerCase() === correctWord.word.toLowerCase()) {
-            return true;
-        }
-        return false;
-    };
+  const checkWord = (word) => {
+    const typedWord = typedWords[word];
+    const correctWord = words.find(item => item.word === word);
+    if (typedWord && correctWord && typedWord.toUpperCase() === correctWord.word) {
+      return true;
+    }
+    return false;
+  };
 
-    const handleInputChange = (word, event) => {
-        setTypedWords({ ...typedWords, [word]: event.target.value });
-    };
+  const handleInputChange = (word, event) => {
+    setTypedWords({ ...typedWords, [word]: event.target.value });
+  };
 
-    const updateScore = (word) => {
-        if (checkWord(word)) {
-            const newScore = score + 1;
-            setScore(newScore);
-            localStorage.setItem('score', newScore);
-            setSuccessWords({ ...successWords, [word]: true });
-            localStorage.setItem('successWords', JSON.stringify({ ...successWords, [word]: true }));
-        }
-    };
+  const updateScore = (word) => {
+    if (checkWord(word)) {
+      const newScore = score + 1;
+      setScore(newScore);
+      localStorage.setItem('score', newScore);
+      setSuccessWords({ ...successWords, [word]: true });
+      localStorage.setItem('successWords', JSON.stringify({ ...successWords, [word]: true }));
+    }
+  };
 
-    return (
-        <div>
-            <NavigationBar />
-            <h1>Words Aloud</h1>
-            <div>
-            <div>
-    <span style={{ color: 'purple' }}>Type</span> the '<span style={{ color: 'blue' }}>Speak Word</span>' button to hear the name of the fruit, then <span style={{ color: 'orange' }}>type</span> it out in the input field and press '<span style={{ color: 'green' }}>check</span>'
-</div>
-
-            <div>
-                <p>Score: {score}</p>
-            </div>
-                {words.map((item, index) => (
-                    <div key={index} style={{ marginBottom: '20px', display: 'inline-block' }}>
-                        <img src={item.image} alt={item.word} style={{ marginRight: '10px', width: '100px', height: '100px' }} />
-                        <span></span> <br></br>
-                        <button onClick={() => speakWord(item.word)} style={{ marginLeft: '10px' }}>
-                            Speak Word
-                        </button> <br></br>
-                        <input
-                            type="text"
-                            placeholder="Type the word here"
-                            value={typedWords[item.word] || ''}
-                            onChange={(e) => handleInputChange(item.word, e)}
-                        />
-                        <button onClick={() => updateScore(item.word)}>
-                            Check
-                        </button>
-                        {successWords[item.word] && <p>Success!</p>}
-                        <br></br>
-                    </div>
-                ))}
-                <br></br>
-            </div>
-            <br></br>
-            <br></br>
-            <br></br>
-            <Footer />
-        </div>
-    );
+  return (
+     <>
+      <NavigationBar />
+      <div style={{ textAlign: 'center', backgroundColor: '#f0f0f0', padding: '20px' }}>
+      
+      <h1 style={{ color: 'blue' }}>Words Aloud</h1>
+      <div>
+        <p>
+          <span style={{ color: 'purple' }}>Type</span> the word, then <span style={{ color: 'orange' }}>speak</span> it out loud and <span style={{ color: 'green' }}>check</span> your answer
+        </p>
+        <p>Score: {score}</p>
+        {words.map((item, index) => (
+          <div key={index} style={{ marginBottom: '20px', display: 'inline-block' }}>
+            <img src={item.image} alt={item.word} style={{ marginRight: '10px', width: '100px', height: '100px' }} />
+            <br />
+            <button onClick={() => speakWord(item.word)} style={{ backgroundColor: '#FF8C00', color: 'white', border: 'none', borderRadius: '5px', padding: '5px 10px', marginTop: '10px' }}>
+              Speak
+            </button>
+            <br />
+            <input
+              type="text"
+              placeholder="Type the word here"
+              value={typedWords[item.word] || ''}
+              onChange={(e) => handleInputChange(item.word, e)}
+              style={{ marginTop: '10px', padding: '5px', borderRadius: '5px', border: '1px solid #ccc' }}
+            />
+            <button onClick={() => updateScore(item.word)} style={{ backgroundColor: '#4CAF50', color: 'white', border: 'none', borderRadius: '5px', padding: '5px 10px', marginTop: '10px', marginLeft: '5px' }}>
+              Check
+            </button>
+            {successWords[item.word] && <p style={{ color: 'green', marginTop: '10px' }}>Success!</p>}
+          </div>
+        ))}
+      </div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Footer />
+    </div>
+    </>
+  );
 }
 
 export default WordLearn;
+
